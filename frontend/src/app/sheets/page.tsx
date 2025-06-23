94c17673683f60b0adf8b16f3289aa448c06cbbd
Michelle <michelleh11219@gmail.com>
2025-06-22 22:41:51 -0400

    Updated: rough skeleton of sheets page

--- frontend/src/app/sheets/page.tsx
+++ frontend/src/app/sheets/page.tsx
@@ -1,7 +1,14 @@
+import Group from '@/app/components/Group';
+
 const Sheets = () => {
+  const arr = ["a", "b", "c"];
+
   return (
     <div>
-      sheets
+      <h1 className="text-2xl">Sheets</h1>
+      {arr.map((item, index) => (
+        <Group key={index} group={item} />
+      ))}
     </div>
   );
 }
