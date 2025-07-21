3fa51b9a8f5a6b37b50ef26b064c42dfca6eb8af
Michelle <michelleh11219@gmail.com>
2025-07-21 16:18:46 -0400

    Modified: Separated user routes into separate file

--- backend/api.py
+++ backend/api.py
@@ -9,76 +9,27 @@ from database.models import User, Group, Sheet, sheet_groups
 from jwt_util import create_token, check_auth_header
 from sqlalchemy import or_
 
+from routes.user_routes import user_bp
+
 load_dotenv()
 
-# Setting up the app and connecting to database
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
-CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:5432/memsheets"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+bcrypt = Bcrypt()
 
-db.init_app(app)
+def create_app():
+    # Setting up the app and connecting to database
+    app = Flask(__name__)
+    
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:5432/memsheets"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
-@app.route("/signup", methods=["POST"])
-def create_user():
-    try:
-        data = request.json
-        if (
-            not data
-            or "username" not in data
-            or "email" not in data
-            or "password" not in data
-        ):
-            return {"error": "Invalid input"}, 400
-        # flask_bcrypt automatically salts and hashes
-        hashed_password = bcrypt.generate_password_hash(data["password"]).decode(
-            "utf-8"
-        )
-        # Create a new user using models.py
-        new_user = User(
-            username=data["username"], email=data["email"], password=hashed_password
-        )
-        db.session.add(new_user)
-        db.session.commit()
-
-        # Create default 'Miscellaneous' group for new user
-        default_group = Group(
-            user_id=new_user.id, name="Miscellaneous", color="#999999"
-        )
-        db.session.add(default_group)
-        db.session.commit()
-
-        return {"message": "User created successfully"}, 201
-    except Exception as e:
-        print(f"Error creating user: {e}")
-        # UNIQUE constraint violation handling for email
-        if "duplicate key value violates unique constraint" in str(e):
-            return {"error": "Email in use"}, 400
-        return {"error": "Registration failed"}, 500
-
-
-@app.route("/login", methods=["POST"])
-def login_user():
-    try:
-        data = request.json
-        if not data or "email" not in data or "password" not in data:
-            return {"error": "Invalid input"}, 400
-
-        # Search for user with the inputted email
-        user = User.query.filter_by(email=data["email"]).first()
-        if user and bcrypt.check_password_hash(user.password, data["password"]):
-            # Create and return JWT to client
-            token = create_token(user.id)
-            return {"token": token}, 200
-        else:
-            return {"error": "Invalid email or password"}, 401
-    except Exception as e:
-        print(f"Error logging in user: {e}")
-        return {"error": "Login failed"}, 500
+    bcrypt.init_app(app)
+    CORS(app)
+    db.init_app(app)
+    
+    app.register_blueprint(user_bp)
+    return app
 
 
 @app.route("/groups", methods=["GET"])
@@ -159,7 +110,7 @@ def get_sheets_by_group(group_id):
         sheets_data = [
             {"id": s.id, "name": s.name, "color": s.color} for s in group.sheets
         ]
-        return { "name": group.name, "color": group.color, "sheets": sheets_data }, 200
+        return {"name": group.name, "color": group.color, "sheets": sheets_data}, 200
     except Exception as e:
         print(f"Error fetching sheets: {e}")
         return {"error": "Fetching sheets failed"}, 500
@@ -172,35 +123,38 @@ def update_group(group_id):
         return {"error": "Invalid token"}, 401
     group = Group.query.filter_by(user_id=user_id, id=group_id).first()
     if not group:
-      return { "error": "Group not found" }, 404
+        return {"error": "Group not found"}, 404
     data = request.json
     for key, value in data.items():
-      # Group names must not be whitespace
-      if key == "name" and not value.strip():
-        setattr(group, key, "Untitled Group")
-      # Add associations between added sheets and correct group
-      
-      elif key == "sheets":
-        # Clear all existing sheet associations for this group
-        group.sheets.clear()
-        
-        # Add all sheets from the updated list
-        for addedSheet in value:
-          # addedSheet has keys color, label, and value
-          sheet = Sheet.query.get(addedSheet["value"])
-          if sheet:
-            group.sheets.append(sheet)
-            
-        # Check for sheets that now belong to no group and add to default group Miscellaneous
-        misc_group = Group.query.filter_by(user_id=user_id, name="Miscellaneous").first()
-        orphaned_sheets = Sheet.query.filter_by(user_id=user_id)\
-          .filter(~Sheet.groups.any()).all()
-        for sheet in orphaned_sheets:
-          misc_group.sheets.append(sheet)
-      else:
-        setattr(group, key, value)
+        # Group names must not be whitespace
+        if key == "name" and not value.strip():
+            setattr(group, key, "Untitled Group")
+        # Add associations between added sheets and correct group
+
+        elif key == "sheets":
+            # Clear all existing sheet associations for this group
+            group.sheets.clear()
+
+            # Add all sheets from the updated list
+            for addedSheet in value:
+                # addedSheet has keys color, label, and value
+                sheet = Sheet.query.get(addedSheet["value"])
+                if sheet:
+                    group.sheets.append(sheet)
+
+            # Check for sheets that now belong to no group and add to default group Miscellaneous
+            misc_group = Group.query.filter_by(
+                user_id=user_id, name="Miscellaneous"
+            ).first()
+            orphaned_sheets = (
+                Sheet.query.filter_by(user_id=user_id).filter(~Sheet.groups.any()).all()
+            )
+            for sheet in orphaned_sheets:
+                misc_group.sheets.append(sheet)
+        else:
+            setattr(group, key, value)
     db.session.commit()
-    return { "message": "Group updated successfully" }, 200
+    return {"message": "Group updated successfully"}, 200
 
 
 @app.route("/new-sheet", methods=["POST"])
@@ -352,4 +306,5 @@ def search_sheet(group_id):
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
---
+++ backend/routes/user_routes.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, request
+from jwt_util import create_token
+from backend.database.models import User, Group
+from backend.api import bcrypt
+from database.db import db
+
+
+user_bp = Blueprint("user_bp", __name__)
+
+@user_bp.route("/signup", methods=["POST"])
+def create_user():
+    try:
+        data = request.json
+        if (
+            not data
+            or "username" not in data
+            or "email" not in data
+            or "password" not in data
+        ):
+            return {"error": "Invalid input"}, 400
+        # flask_bcrypt automatically salts and hashes
+        hashed_password = bcrypt.generate_password_hash(data["password"]).decode(
+            "utf-8"
+        )
+        # Create a new user using models.py
+        new_user = User(
+            username=data["username"], email=data["email"], password=hashed_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Create default 'Miscellaneous' group for new user
+        default_group = Group(
+            user_id=new_user.id, name="Miscellaneous", color="#999999"
+        )
+        db.session.add(default_group)
+        db.session.commit()
+
+        return {"message": "User created successfully"}, 201
+    except Exception as e:
+        print(f"Error creating user: {e}")
+        # UNIQUE constraint violation handling for email
+        if "duplicate key value violates unique constraint" in str(e):
+            return {"error": "Email in use"}, 400
+        return {"error": "Registration failed"}, 500
+
+
+@user_bp.route("/login", methods=["POST"])
+def login_user():
+    try:
+        data = request.json
+        if not data or "email" not in data or "password" not in data:
+            return {"error": "Invalid input"}, 400
+
+        # Search for user with the inputted email
+        user = User.query.filter_by(email=data["email"]).first()
+        if user and bcrypt.check_password_hash(user.password, data["password"]):
+            # Create and return JWT to client
+            token = create_token(user.id)
+            return {"token": token}, 200
+        else:
+            return {"error": "Invalid email or password"}, 401
+    except Exception as e:
+        print(f"Error logging in user: {e}")
+        return {"error": "Login failed"}, 500
